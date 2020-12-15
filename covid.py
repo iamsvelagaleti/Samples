d94b86c15fde1f2ff35433d1358e6fc901114a6a
Sandeep Velagaleti <59356235+iamsvelagaleti@users.noreply.github.com>
2020-12-15 12:23:32 +0530

    Update covid.py

--- covid.py
+++ covid.py
@@ -3,15 +3,18 @@ from bs4 import BeautifulSoup
 
 def lambda_caller():
     response = {}
+    response['statusCode'], response['headers'], response['body'] = 200, {}, {}
+    response['headers']['Content-Type'] = 'application/json'
+    
     url = 'https://www.worldometers.info/coronavirus/countries-where-coronavirus-has-spread/'
     html = BeautifulSoup(requests.get(url).text, 'html5lib')
 
     for index, each in enumerate(html.find_all('tr')[1:]):
         each = each.find_all('td')
-        response[index+1] = {
+        response['body'][index+1] = {
             'country' : each[0].text,
             'cases' : each[1].text,
             'deaths' : each[2].text,
             'continent' : each[3].text
-        } 
-    print(response)
+        }
+    return response
